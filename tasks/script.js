29b7e45c62fc62689938dfdb186885a1faa67300
andrey <andrey@edu.yar.ru>
2016-10-13 11:30:39 +0300

    Fixed dependencies, moved to yarn, updated typings.

--- tasks/script.js
+++ tasks/script.js
@@ -1,14 +1,11 @@
 var ts = require('gulp-typescript');
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var config = require('./config');
 
 var tsProject = ts.createProject("./tsconfig.json");
 
 module.exports = function() {
 	var tsResult = gulp.src(config.script.src)
-        .pipe(sourcemaps.init())
         .pipe(tsProject())
-        .pipe(sourcemaps.write(config.script.map))
         .pipe(gulp.dest(config.script.dst));
-}
+};
