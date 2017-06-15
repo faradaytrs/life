33c28e34e96d415f1a65a05701e8683361cc0ba9
andrey <andrey@edu.yar.ru>
2017-06-15 16:06:01 +0300

    attempt to fix bug

--- src/ts/rules.ts
+++ src/ts/rules.ts
@@ -2,23 +2,25 @@ import {Car} from "./car";
 import {Cell, Direction, Type} from "./cell";
 
 export const rules = {
-    classic: (currentField, nextField, cell, nextCell) => {
+    classic: (currentField, nextField, cell, cellAhead) => {
+
+    	const newCell = nextField[cell.y][cell.x];
 	
 		if (cell.trafficLight != null) {
-			cell.trafficLight.counter += 1; // 
+			newCell.trafficLight.counter = cell.trafficLight.counter + 1; //
 			
 			//console.log(cell.trafficLight);
 			
 			if (cell.trafficLight.isRed && cell.trafficLight.counter >= cell.trafficLight.redDuration) {
-				cell.trafficLight.counter = 0;
-				cell.trafficLight.isRed = false;
+				newCell.trafficLight.counter = 0;
+				newCell.trafficLight.isRed = false;
 			} else if (!cell.trafficLight.isRed && cell.trafficLight.counter >= cell.trafficLight.greenDuration) {
-				cell.trafficLight.counter = 0;
-				cell.trafficLight.isRed = true;
+				newCell.trafficLight.counter = 0;
+				newCell.trafficLight.isRed = true;
 			}
 		}
 	
-		if (nextCell == null) {
+		if (cellAhead == null) {
 			//nextField[cell.y][cell.x].car = null;
 			return;
 		}
@@ -34,18 +36,18 @@ export const rules = {
 		// Check if there's a car on this cell
         if (cell.car) {
 			
-			if (nextCell.car == null && nextField[nextCell.y][nextCell.x].car == null) {
+			if (cellAhead.car == null && nextField[cellAhead.y][cellAhead.x].car == null) {
 			
 				// The car is moving
-				cell.car.counter += cell.car.speed;
+				newCell.car.counter += cell.car.speed;
 				
 				console.log(cell.car);
 			
 				if (cell.car.counter >= 100) {
-					nextField[nextCell.y][nextCell.x].car = cell.car;
+					nextField[cellAhead.y][cellAhead.x].car = cell.car;
 					nextField[cell.y][cell.x].car = null;
-					
-					cell.car.counter -= 100;
+
+					newCell.car.counter -= 100;
 				}
 			} else {
 				// The car is not moving due to another car being in the way
